eb56faa4981523c840a9a72eba45e81079bf5785
Minsung_Kim <alstjd025@naver.com>
2023-04-17 14:11:34 +0000

    Change to IPC
    Changes to be committed:
    	modified:   tensorflow/lite/lite_runtime.h

--- tensorflow/lite/lite_runtime.h
+++ tensorflow/lite/lite_runtime.h
@@ -10,6 +10,7 @@
 #include "condition_variable"
 #include "opencv2/opencv.hpp"
 #include "tensorflow/lite/interpreter.h"
+#include "tensorflow/lite/core/subgraph.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
 #include "tensorflow/lite/optional_debug_tools.h"
