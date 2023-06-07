f3f65e4a47b48be0b62b73ce8271602e5b527035
Minsung Kim <alstjd025@naver.com>
2023-06-07 13:45:01 +0900

    fix p plan

--- tensorflow/lite/tf_scheduler.cc
+++ tensorflow/lite/tf_scheduler.cc
@@ -264,8 +264,8 @@ void TfScheduler::CreatePartitioningPlan(tf_packet& rx_p, tf_packet& tx_p){
     // if want two subgraph
     tx_p.partitioning_plan[0][TF_P_IDX_START]    = 0;
     tx_p.partitioning_plan[0][TF_P_IDX_END]      = 4;
-    tx_p.partitioning_plan[0][TF_P_IDX_RESOURCE] = TF_P_PLAN_CO_E;
-    tx_p.partitioning_plan[0][TF_P_IDX_RATIO]    = 15; // partitioning ratio
+    tx_p.partitioning_plan[0][TF_P_IDX_RESOURCE] = TF_P_PLAN_CPU;
+    tx_p.partitioning_plan[0][TF_P_IDX_RATIO]    = 0; // partitioning ratio
     tx_p.partitioning_plan[1][TF_P_IDX_START]    = 4;
     tx_p.partitioning_plan[1][TF_P_IDX_END]      = 9;
     tx_p.partitioning_plan[1][TF_P_IDX_RESOURCE] = TF_P_PLAN_CPU;
